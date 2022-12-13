ad6732942b5a60c5fd0ebbbdbae12b637a647a8f
CHIU <78676302+Raito1995@users.noreply.github.com>
2022-12-13 19:54:48 +0800

    update

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const line = require('@line/bot-sdk');
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-const openai = new OpenAIApi(configuration);
-
-
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -36,21 +28,14 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-async function handleEvent(event) {
+function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: event.message.text ,
-    max_tokens: 200,
-  });
-
   // create a echoing text message
-  const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
-
+  const echo = { type: 'text', text: event.message.text };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
