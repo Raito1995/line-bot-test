c8ad98df376cd07ab410c5ee0c0ea59fdf367e0b
raito95 <raitochiu@jorjin.com.tw>
2022-12-14 18:01:46 +0800

    update

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ async function handleEvent(event) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: event.message.text ,
-    max_tokens: 500,
+    max_tokens: 512,
   });
   if(event.message.text[0] === "!") {
     echo = { type: 'text', text: completion.data.choices[0].text.trim() };
